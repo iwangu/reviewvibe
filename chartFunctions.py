52d2957b277fef1280aa818323d945f6016ad255
iwan <iwang@fastmail.net>
2014-07-13 11:38:03 +0200

    init

---
+++ chartFunctions.py
@@ -0,0 +1,75 @@
+bgcolor = "'" + "#f0f0f0" + "'"
+
+def getHTMLFromDataBarChart(endlist1, endlist2):
+
+    total1 = endlist1[1] + endlist1[2] + endlist1[3]
+    total2 = endlist2[1] + endlist2[2] + endlist2[3]
+
+    width = "530px"
+    height = "500px" 
+
+    #title1 = endlist1[0]
+    #title2 = endlist2[0]
+
+    endlist1[0] ="(" + str(int(total1)) + " total) " + endlist1[0].replace("-", " ")
+    endlist2[0] ="(" + str(int(total2)) + " total) " + endlist2[0].replace("-", " ")
+
+    listOfStuff = []
+    listOfStuff.append(['Device', 'Positive', 'Negative', 'Neutral'])
+    listOfStuff.append(endlist1)
+    listOfStuff.append(endlist2)
+
+    
+    fulltitle =  "" #title1 + ''' vs. ''' + title2 
+
+    html = '''<script type="text/javascript" src="https://www.google.com/jsapi"></script> <script type="text/javascript">  google.load("visualization", "1", {packages:["corechart"]});  google.setOnLoadCallback(drawChart);  function drawChart() { var data = google.visualization.arrayToDataTable(''' + str(listOfStuff) + '''); var options = {width: 530, height: 500,backgroundColor: ''' + bgcolor + ''',   title: ' ''' + fulltitle + ''' ',  hAxis: {title: '', titleTextStyle: {color: 'red'}} }; var chart = new google.visualization.ColumnChart(document.getElementById('chart_div')); chart.draw(data, options);  } </script> <div id="chart_div" style="width: ''' + width + '''; height: ''' + height + ''';"></div> '''
+
+    return html
+
+def getHTMLFromDataDonut(list1, list2):
+	#preprocessing
+	#s1 =  [["Positive", 60.0], ["Negative",40.0] , ["Neutral",10.0]]
+    #s2 =  [["Positive", 60.0], ["Negative",40.0] , ["Neutral",10.0]] 
+
+    title1 = list1.pop(0) 
+    title2 = list2.pop(0)
+    
+    endlist1 = []
+    endlist2 = []
+    
+    endlist1.append(["Positive", list1[0]])
+    endlist1.append(["Negative", list1[1]])
+    endlist1.append(["Neutral", list1[2]]) 
+
+    endlist2.append(["Positive", list2[0]])
+    endlist2.append(["Negative", list2[1]])
+    endlist2.append(["Neutral", list2[2]]) 
+
+    total1 = list1[0] + list1[1] + list1[2]
+    total2 = list2[0] + list2[1] + list2[2]
+   
+    width = "400px"
+    height = "480px"
+     
+    endlist1.insert(0,['Fraction', 'Positivity'])
+    titleTxt1 = "'(" + str(int(total1)) + " total) " + title1.replace("-", " ") + "'"
+    titleId1 = title1 + "'"
+ 
+     
+    endlist2.insert(0,['Fraction', 'Positivity'])
+    titleTxt2 = "'(" + str(int(total2)) + " total) " + title2.replace("-", " ") + "'" 
+    titleId2 = title2 + "'"
+
+    html = '''<script type="text/javascript" src="https://www.google.com/jsapi"></script> <script type="text/javascript">  google.load("visualization", "1", {packages:["corechart"]});  google.setOnLoadCallback(drawChart);  function drawChart() { var data = google.visualization.arrayToDataTable(''' + str(endlist1) + ''' ); var options = { backgroundColor: ''' + bgcolor + ''',   title: ''' + titleTxt1 + ''',   pieHole: 0.4, }; var chart = new google.visualization.PieChart(document.getElementById('donutchart''' + titleId1 + ''')); chart.draw(data, options);  }    </script> ''' + '''<script type="text/javascript" src="https://www.google.com/jsapi"></script> <script type="text/javascript">  google.load("visualization", "1", {packages:["corechart"]});  google.setOnLoadCallback(drawChart);  function drawChart() { var data = google.visualization.arrayToDataTable(''' + str(endlist2) + ''' ); var options = {backgroundColor: ''' + bgcolor + ''',    title: ''' + titleTxt2 + ''',   pieHole: 0.4, }; var chart = new google.visualization.PieChart(document.getElementById('donutchart''' + titleId2 + ''')); chart.draw(data, options);  }    </script> '''  + '''<div><div id="donutchart'''+title1+'''"  style="display: inline-block;height:''' + height + '''; width:''' + width + ''';"> </div><div id="donutchart'''+title2+'''"  style="display: inline-block;height:''' + height + '''; width:''' + width + ''';"> </div></div>'''
+    return html
+
+
+
+
+s1 = ["Apple Ipad version xy", 111,222,333]
+s2 = ["Nexus 7 version xy", 151,222,333]
+
+#html = getHTMLFromDataBarChart(s1,s2) + getHTMLFromDataDonut(s1,s2)
+f = open('chart2.html','w')
+#f.write(html) # python will convert \n to os.linesep
+#f.close() # you can omit in most cases as the destructor will call if
---
+++ grabkimono.py
@@ -0,0 +1,220 @@
+from textblob import TextBlob
+import time
+import io
+import urllib 
+import json 
+import requests
+from pprint import pprint
+from textblob import TextBlob 
+import chartFunctions
+import random
+
+def grabkimono(products): 
+   #products = ["http://www.amazon.com/Apple-MD199LL-A-TV/dp/B007I5JT4S", "http://www.amazon.com/Google-Chromecast-Streaming-Media-Player/dp/B00DR0PDNE"]  for s in products:
+    for s in products:
+        productName = s.split("/")[3] 
+        idOfProduct = s.split("/")[5] 
+        grabNoOfPages = 10     
+        counter = 1
+        for x in range(1,grabNoOfPages+1):
+            api = "https://www.kimonolabs.com/api/b6mvxgxs?apikey=68d2fb6d1d7f5448161a279564ed03fc" +"&kimpath1=" + productName + "&kimpath2=" + "product-reviews" + "&kimpath3=" + idOfProduct + "&kimpath4=" + "ref=cm_cr_pr_top_link_"+ str(x) + "&pageNumber="+ str(x)
+            print api
+
+            time.sleep(1) # delays for 5 seconds
+            results =""
+            print results 
+         
+            r = requests.get(api)
+            with open("data/"  + str(counter) + productName +  ".txt", 'wb') as fd:
+                for chunk in r.iter_content(10):
+                    fd.write(chunk)
+            counter +=1
+
+
+def read(f): 
+    with open(f) as data_file:    
+        data = json.load(data_file)
+        #pprint(data)
+        return data
+
+
+
+def isbogus(mystring):
+    if len(mystring.split()) < 3:
+        #print "++++++++++bogus: " + mystring
+        return True
+    bogusSentences = ["This review is from:", "PermalinkComment","Was this review helpful to you"]
+    for x in bogusSentences:
+        if x in mystring:
+            #print "++++++++++bogus: " + mystring
+            return True 
+    #print  mystring
+    return False 
+
+def parseFiles(pLink,grabNoOfPages):        
+
+    pName = pLink.split("/")[3] 
+
+    ssum = 0.0
+    psum = 0.0
+    total = 0.0
+    for counter in range(1,grabNoOfPages+1):
+
+        fpath = "data/" + str(counter) + pName + ".txt"
+        f = read(fpath) 
+
+        for comment in f['results']['collection1']: 
+            txt =""
+            try: 
+                txt = comment['property1']['text'].strip()
+            except:
+                txt = comment['property1'].strip()
+
+            if(isbogus(txt)):
+                continue
+
+            pprint(txt)
+            txtBlob = TextBlob(txt)
+            p = float(txtBlob.sentiment.polarity)
+            s = float(txtBlob.sentiment.subjectivity)
+            #print str(p)  +  " "   + str(s)
+            ssum += s
+            psum += p
+            total += 1.0
+
+    print "total avg polar: " + str(psum/total)
+    print "total avg subj: " + str(ssum/total)
+    print "total comments: " + str(total)
+
+def parseFilesSentences(pLink,grabNoOfPages):
+    pName = pLink.split("/")[3] 
+    ssum = 0.0
+    psum = 0.0
+    total = 0.0
+
+    posBin = 0
+    neutBin = 0
+    negBin = 0 
+
+    res = []
+    res.append(pName)
+
+    for counter in range(1,grabNoOfPages+1):
+
+        fpath = "data/" + str(counter) + pName + ".txt"
+        f = read(fpath) 
+
+        for comment in f['results']['collection1']: 
+            txt =""
+            try: 
+                txt = comment['property1']['text'].strip()
+            except:
+                txt = comment['property1'].strip()
+
+            if(isbogus(txt)):
+                continue
+                
+            #pprint(txt)
+            txtBlob = TextBlob(txt)
+            for sentence in txtBlob.sentences: 
+                p = float(sentence.sentiment.polarity)
+                s = float(sentence.sentiment.subjectivity)
+                #print str(p)  +  " "   + str(s)
+                ssum += s
+                psum += p
+                total += 1.0
+
+                if p == 0.0:
+                    neutBin += 1.0
+                elif p < 0.0:
+                    negBin += 1.0
+                else:
+                    posBin += 1.0
+
+    print "total avg polar: " + str(psum/total)
+    print "total avg subj: " + str(ssum/total)
+    print "total comments: " + str(total) 
+    print "total posbin: " + str(posBin)
+    print "total negbin: " + str(negBin)
+    print "total neutbin: " + str(neutBin)
+    res.append(posBin)
+    res.append(negBin)
+    res.append(neutBin)
+    return res
+
+def compareAndPrint(p1,p2,pNo1,pNo2):
+    pName   = p1.split("/")[3] 
+    pName2   = p2.split("/")[3]   
+
+    s1=parseFilesSentences(pName,pNo1)
+    total1 = s1[1] + s1[2] + s1[3] 
+
+    s2=parseFilesSentences(pName2,pNo2) 
+    total2 = s2[1] + s2[2] + s2[3] 
+
+    #nachjustieren
+    diff = (total1 - total2)
+    if abs(diff) > 30:
+        s2 = parseFilesSentences(pName2,pNo2 + int(diff/70))
+        print "nachjustierung: " + str(int(diff/70))
+
+    html = chartFunctions.getHTMLFromDataDonut(s1,s2)
+    #html = "<br>"
+    #html += chartFunctions.getHTMLFromDataBarChart(s1,s2)
+    f = open(pName + '-vs.-' + pName2 + '-chart.html','w')
+    f.write(html) # python will convert \n to os.linesep
+    f.close() # you can omit in most cases as the destructor will call if
+    print "+++++++++++DONE+++++++++++++++++++++"
+
+def parseTop100Files(fpath):  
+    l = []
+    f = read(fpath) 
+
+    for i in f['results']['twenty']: 
+        txt =""
+
+        txt = i['title']['href'].strip()
+        txt = txt.split("\n\n\n\n\n\n\n")[1] 
+        l.append(txt)
+        #pprint(txt)  
+    return l
+
+def goParse100():
+    tmp = parseTop100Files("data/top100_11.07.json")
+    res = []
+    #print len(tmp) 
+    #r1 =  tmp[random.randint(1, len(tmp))]
+    #r2 =  tmp[random.randint(1, len(tmp))]
+     
+    for x in tmp:
+        #print x
+        res.append(x)
+    return res
+
+#p = ["http://www.amazon.com/Google-Chromecast-Streaming-Media-Player/dp/B00DR0PDNE/ref=zg_bs_electronics_1/184-6199652-9954344","http://www.amazon.com/Roku-3-Streaming-Media-Player/dp/B00BGGDVOO/ref=zg_bs_electronics_7/184-6199652-9954344"]
+
+#grabkimono(p)
+
+#compareAndPrint(p[0],p[1],10,10)
+
+#res = goParse100()
+#for x in res:
+#    compareAndPrint
+
+def compareAll():
+    for x, left in enumerate(res):
+        print x
+        print left
+
+
+
+#products = ["http://www.amazon.com/Apple-MD788LL-Silver-NEWEST-VERSION/dp/B00G2Y4WNY/ref=sr_1_1?s=electronics&ie=UTF8&qid=1404912902&sr=1-1&keywords=ipad+air","http://www.amazon.com/Google-Nexus-10-Wi-Fi-only/dp/B00ACVI202/"]
+#grabkimono()
+#print len( parseTop100Files("data/top100_11.07.json")[0])
+#play()
+#parseFiles(pName,10)
+#parseFilesSentences(pName,10)
+#print read('data/1Apple-MD788LL-Silver-NEWEST-VERSION.txt')
+#grabkimono()
+
+#s1 = ["Apple Ipad version xy", 111,222,333]
